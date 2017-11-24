ffcc4a56eb91278a2052b9b5e841f6ef059dfa5b
Jesse Onolememen <jesster2k10@gmail.com>
2017-11-24 22:30:01 +0000

    Updated formula

--- helpers/orders.js
+++ helpers/orders.js
@@ -10,6 +10,7 @@ exports.calculateTimeToDeliverOrder = (order) => new Promise (async (resolve, re
 		let totalAvaliableStaff = await restaurant.getAvaliableStaff().length;
 		let storeAddress = restaurant.getStoreAddress();
 		let baseTime = restaurant.getBaseTime();
+		let productsCount = order.products.length;
 		
 		if (totalPendingOrders == 0 || !totalPendingOrders) {
 			totalPendingOrders = 1;
@@ -19,17 +20,21 @@ exports.calculateTimeToDeliverOrder = (order) => new Promise (async (resolve, re
 			totalAvaliableStaff = 1;
 		}
 		
+		if (productsCount === 0 || !productsCount) {
+			productsCount = 1;
+		}
+		
 		if (order.type == 'Delivery' || order.type == 'DELIVERY') {
 			let distance = await locations.calcDistanceBetween(locations.formatAddress(order.deliveryAddress), storeAddress);
 			
-			const deliveryWait = ( (totalPendingOrders * 10) ) / ( 10 * (1.5 * totalAvaliableStaff)) + (distance / 1.25) + baseTime ;
+			const deliveryWait = ( (totalPendingOrders * 10) ) / ( 10 * (1.5 * totalAvaliableStaff)) + (distance / 1.25) + baseTime + ((baseTime/3) * productsCount);
 			
 			resolve(Math.round(deliveryWait));
 			
 			return;
 		}
 		
-		const collectionWait = (totalPendingOrders * 10 * baseTime) / (10 * (1.5 * totalAvaliableStaff));
+		const collectionWait = (totalPendingOrders * 10) / (10 * (1.5 * totalAvaliableStaff)) + baseTime + ((baseTime/3) * productsCount);
 		resolve(Math.round(collectionWait));
 	} catch (error) {
 		reject(error);
